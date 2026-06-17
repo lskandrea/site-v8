447a8ed2a02dd1758d3f0d8d340f47073a2c18f3
lskandrea <157403455+lskandrea@users.noreply.github.com>
2026-06-17 20:15:32 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# site-v8
+# MJSC Judo Manosque - V8 multipages
+
+Site statique prêt pour Cloudflare Pages.
+
+Fichiers principaux :
+- index.html
+- judo.html
+- bjj.html
+- taiso.html
+- horaires.html
+- galerie.html
+- inscription.html
+- contact.html
+- styles.css
+- app.js
+- config.js
+- assets/logo-mjsc.png
+
+À modifier plus tard dans `config.js` : email, téléphone, réseaux sociaux.
---
+++ config.js
@@ -0,0 +1,13 @@
+window.MJSC_CONFIG = {
+  clubName: "MJSC Judo Manosque",
+  address: "Lycée des Iscles, Manosque",
+  email: "contact@mjsc-judo.fr",
+  phone: "06 00 00 00 00",
+  googleMapsUrl: "https://www.google.com/maps/search/?api=1&query=Lyc%C3%A9e%20des%20Iscles%20Manosque",
+  googleCalendarEmbedUrl: "https://calendar.google.com/calendar/embed?src=fr.french%23holiday%40group.v.calendar.google.com&ctz=Europe%2FParis",
+  googleFormEmbedUrl: "https://docs.google.com/forms/d/e/1FAIpQLScWsSrNFTJjSfwFfQDwoCRDX1ryQ0OJIu_pEe0zPilvZBVaEA/viewform?embedded=true",
+  googleDriveFolderUrl: "https://drive.google.com/drive/folders/1xPzKkZ_tdigLaPH5XZI_ixVt4WO3Wf5f?usp=drive_link",
+  facebookUrl: "https://www.facebook.com/friends/suggestions/?profile_id=100006378492702",
+  instagramUrl: "https://www.instagram.com/colin_serge_mjsc/",
+  youtubeUrl: "#"
+};
